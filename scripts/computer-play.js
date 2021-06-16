52b48ab62178a107ddc87afe6c0dda4912b91536
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2021-06-16 19:05:22 -0400

    Introduce computerPlay function

---
+++ scripts/computer-play.js
@@ -0,0 +1,8 @@
+function computerPlay() {
+    //Create an array for all of the computer's possible selections
+    const OPTIONS = ["Rock", "Paper", "Scissors"];
+    //Randomly generate the computer's selection
+    let selection = OPTIONS[Math.floor(Math.random() * OPTIONS.length)];
+    //Return the computer's selection
+    return selection;
+}
